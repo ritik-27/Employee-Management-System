f3598552c08d7cb1df49a32fb4f46427c8bf32c9
Ritik Jain <ritikjain666@gmail.com>
2022-04-18 19:13:08 +0530

    component setup

--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
+import List from './List'
+import Add from './Add'
+import Edit from './Edit'
+import { employeesdata } from '../../data/index'
 
-function Index() {
+function Dashboard() {
+
+    const [employees, setEmployees] = useState(employeesdata)
+    const [isAdding, setisAdding] = useState(false)
+    const [isEditing, setIsEditing] = useState(false)
+    const [selectedEmployee, setSelectedEmployee] = useState(null)
+
+
+    const handleEdit = () => {
+        //TODO
+    }
+
+    const handledelete = () => {
+        //TODO
+    }
+
     return (
-        <div>Index</div>
+        <div className="container">
+
+            {/*Showing List */}
+            {!isAdding && !isEditing && (
+                <>
+                    <Header
+                        setisAdding={setisAdding}
+                    />
+                    <List
+                        employees={employees}
+                        handleEdit={handleEdit}
+                        handledelete={handledelete}
+                    />
+                </>
+            )}
+
+            {/* Showing Add */}
+            {isAdding && (
+                <Add
+                    employees={employees}
+                    setEmployees={setEmployees}
+                    setisAdding={setisAdding}
+                />
+            )}
+
+            {/* Showing Edit */}
+            {isEditing && (
+                <Edit
+                    employees={employees}
+                    setEmployees={setEmployees}
+                    setIsEditing={setIsEditing}
+                    selectedEmployee={selectedEmployee}
+                />
+            )}
+        </div>
     )
 }
 
-export default Index
+export default Dashboard
